63ee9ccbff7a220c64bb0bc2420a35282727d454
Parker <paross@mtu.edu>
2011-07-14 14:13:38 -0400

    implemented permissions group changing

--- MagicWord/src/MagicWord.java
+++ MagicWord/src/MagicWord.java
@@ -1,9 +1,10 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.List;
-//import java.util.Map;
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,14 +14,17 @@ import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
-//import org.bukkit.util.config.ConfigurationNode;
+
 
 
 public class MagicWord extends JavaPlugin{
 	Configuration config;
 	String word;
-	ArrayList<Player> builders = new ArrayList<Player>();
 	ArrayList<Player> admins = new ArrayList<Player>();
+	public static PermissionHandler permissionHandler;
+	PluginManager pm;
+	File permissionFile;
+	Configuration permConfig;
 
 	@Override
 	public void onDisable() {
@@ -30,10 +34,8 @@ public class MagicWord extends JavaPlugin{
 
 	@Override
 	public void onEnable() {
-		PluginManager pm = getServer().getPluginManager();
+		pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_COMMAND_PREPROCESS, new CommandListener(this), Priority.Normal, this);
-		pm.registerEvent(Event.Type.BLOCK_PLACE, new BuildListener(this), Priority.High, this);
-		pm.registerEvent(Event.Type.BLOCK_DAMAGE, new BuildListener(this), Priority.High, this);
 
 		new File("plugins/MagicWord").mkdir();
 		File data = new File("plugins/MagicWord/config.yml");
@@ -54,35 +56,29 @@ public class MagicWord extends JavaPlugin{
 		if(!config.getKeys().contains("Magic Word")){
 			config.setProperty("Magic Word", "Please");
 		}
-		
-		if(!config.getKeys().contains("Allowed Players")){
-			List<String> name = new ArrayList<String>();
-			name.add("JorganPubshire");
-			config.setProperty("Allowed Players", name);
-		}
+
 		if(!config.getKeys().contains("Admins/Mods")){
 			List<String> name = new ArrayList<String>();
 			name.add("JorganPubshire");
 			config.setProperty("Admins/Mods", name);
 		}
-		
+
 		config.save();
 
 		List<String> names = config.getStringList("Admins/Mods", null);
-		
+
 		for(String name : names){
 			admins.add(getServer().getPlayer(name));
 		}
-		
-		names = config.getStringList("Allowed Players", null);
-		
-		for(String name : names){
-			builders.add(getServer().getPlayer(name));
-		}
-		
 
 		word = config.getString("Magic Word","");
 
+		setupPermissions();
+
+		if(permissionHandler != null){
+			permissionFile = new File("plugins/Permissions/" + getServer().getWorlds().get(0).getName() + ".yml");
+			permConfig = new Configuration(permissionFile);
+		}
 
 		System.out.println("MagicWord is working.");
 		System.out.println("The Magic Word is: " + word);
@@ -91,23 +87,27 @@ public class MagicWord extends JavaPlugin{
 	public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args){
 		Player player = (Player) sender;
 		player.toString();
-		if(cmdLabel.equalsIgnoreCase("allow")){
-			if(args.length == 1){
-				if(admins.contains(player)){
+		if (cmdLabel.equalsIgnoreCase("group")){
+			if(admins.contains(player)){
+				if(args.length == 0){
+					permConfig.load();
+					List<String> keys = permConfig.getKeys("groups");
+					for(int i = 0; i<keys.size();i++){
+						player.sendMessage(keys.get(i));
+					}
+				}
+				if(args.length == 1){
+					permConfig.load();
+					player.sendMessage(getServer().getPlayer(args[0]).getName() + " is in the group: " + permConfig.getString("users." + getServer().getPlayer(args[0]).getName() + ".group"));
+				}
+				if(args.length == 2){
 					Player target = getServer().getPlayer(args[0]);
-					allowPlayer(target);
-					getServer().broadcastMessage(ChatColor.GREEN + player.getName() + " has granted building rights to " + target.getName());
+					String group = args[1];
+					setGroup(player,target,group);
 				}
 			}
 		}
-		else if(cmdLabel.equalsIgnoreCase("deny")){
-			if(args.length == 1){
-				Player target = getServer().getPlayer(args[0]);
-				denyPlayer(target);
-				getServer().broadcastMessage(ChatColor.RED + target.getName() + " has lost building rights!");
-			}
-		}
-		
+
 		else if(cmdLabel.equalsIgnoreCase("word")){
 			if(admins.contains(player)){
 				if(args.length == 0){
@@ -117,46 +117,58 @@ public class MagicWord extends JavaPlugin{
 					setWord(args[0]);
 					player.chat("/word");
 				}
-				
+
 			}
 		}
 		return true;
 	}
-	
-	public void allowPlayer(Player player){
-		config.load();
-		if(!builders.contains(player)){
-			builders.add(player);
-			config.setProperty("Allowed Players", config.getStringList("Allowed Players", null).add(player.getName()));
-		}
-			config.save();
-	}
-	
-	public void denyPlayer(Player player){
-		config.load();
-		if(builders.contains(player)){
-			builders.remove(player);
-			config.setProperty("Allowed Players", config.getStringList("Allowed Players", null).remove(player.getName()));
-		}
-		config.save();
-	}
-	
+
 	public void setWord(String word){
 		config.load();
 		config.setProperty("Magic Word", word);
 		config.save();
 		this.word = word;
-		
+
 	}
-	
+
 	public String getWord(){
 		return word;
 	}
-	
+
 	public void messageAdmins(String message){
 		for(Player name : admins){
 			name.sendMessage(ChatColor.YELLOW + message);
 		}
 	}
 
+	private void setupPermissions() {
+		if (permissionHandler != null) {
+			return;
+		}
+
+		Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+
+		if (permissionsPlugin == null) {
+			System.out.println("[MagicWord]Permission system not detected, defaulting to OP");
+			return;
+		}
+
+		permissionHandler = ((Permissions) permissionsPlugin).getHandler();
+		System.out.println("[MagicWord]Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+	}
+
+	public void setGroup(Player sender, Player player, String group){
+		permConfig.load();
+		if(permConfig.getKeys("users")!=null && !permConfig.getKeys("users").contains(player.getName())){
+			permConfig.setProperty("users." + player.getName() + ".group", group);
+			permConfig.setProperty("users." + player.getName() + ".permissions", "");
+		}
+		else{
+			permConfig.setProperty("users." + player.getName() + ".group", group);
+		}
+		permConfig.save();
+		sender.chat("/permissions -reload all");
+		permissionHandler = null;
+		setupPermissions();
+	}
 }
